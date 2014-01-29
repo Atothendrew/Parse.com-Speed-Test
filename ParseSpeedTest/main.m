c6757eeae48792ba33a61db4d8d992b880de3efd
Andrew Williamson <atothendrew24@mac.com>
2014-01-29 16:20:37 -0700

    Initial Commit

---
+++ ParseSpeedTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParseSpeedTest
+//
+//  Created by Andrew Williamson on 1/29/14.
+//  Copyright (c) 2014 Andrew Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ParseSpeedTestTests/ParseSpeedTestTests.m
@@ -0,0 +1,34 @@
+//
+//  ParseSpeedTestTests.m
+//  ParseSpeedTestTests
+//
+//  Created by Andrew Williamson on 1/29/14.
+//  Copyright (c) 2014 Andrew Williamson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ParseSpeedTestTests : XCTestCase
+
+@end
+
+@implementation ParseSpeedTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
